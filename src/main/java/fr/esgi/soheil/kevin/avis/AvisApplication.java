5b39b165a455ec1a12e32ceea52379e7f8adf432
soheil <senisonn@gmail.com>
2026-03-30 14:18:44 +0200

    Initial commit

---
+++ src/main/java/fr/esgi/soheil/kevin/avis/AvisApplication.java
@@ -0,0 +1,13 @@
+package fr.esgi.soheil.kevin.avis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvisApplication.class, args);
+	}
+
+}
